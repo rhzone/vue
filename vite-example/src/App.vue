9d76139fa492eff03bdf10c345fe4ee56f146a84
rhzone <atgdut@gmail.com>
2022-04-11 09:13:32 +0800

    chore: 更新 flow 和使用 pnpm

---
+++ vite-example/src/App.vue
@@ -0,0 +1,14 @@
+
+
+<script>
+import Vue from 'vue'
+window.Vue = Vue
+export default {
+  template: '<div @click="foo">123{{ msg }}</div>',
+  data() {
+    return {
+      msg: '123'
+    }
+  },
+}
+</script>
